3d7d239c95b6277229d4771a769676c41ba150ed
PATHAN RAYYAN KHAN (Rayyu) <143524922+Rayyankhaan@users.noreply.github.com>
2026-01-29 21:53:11 +0530

    Delete security/device directory

--- security/device/ble_discovery.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def advertise():
-    print("BLE advertising started")
-
-def discover():
-    print("Searching nearby devices...")
-    return "192.168.49.1"  # Wi-Fi Direct IP placeholder
