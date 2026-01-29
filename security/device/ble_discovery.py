6da6cd235b0ff9a262956cc3f4372f0f05c70c84
PATHAN RAYYAN KHAN (Rayyu) <143524922+Rayyankhaan@users.noreply.github.com>
2026-01-29 21:46:49 +0530

    Create ble_discovery.py

---
+++ security/device/ble_discovery.py
@@ -0,0 +1,8 @@
+import socket
+
+def advertise():
+    print("BLE advertising started")
+
+def discover():
+    print("Searching nearby devices...")
+    return "192.168.49.1"  # Wi-Fi Direct IP placeholder
